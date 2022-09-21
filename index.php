bbc22a8d4f4f43eee9c492bc792c06714e442620
unknown <tomasz.piekarski@live.zs1mm.edu.pl>
2022-09-21 09:38:45 +0200

    zad2.2

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 $connect = file_get_contents('http://loripsum.net/api');
-$array = explode(' ', $connect);
-foreach ($array as $we){
-    if (preg_match("/\b(\w*e\w*)\b/ ", $we, $z) == true){
-        echo $z[0]."<br>";
+$wyszukaj="l";
+function ArraySort($tablica, $szukaj){
+    $powrot;
+    $tablica = explode(' ', $tablica);
+    sort($tablica, SORT_NATURAL | SORT_FLAG_CASE);
+    foreach ($tablica as $we){
+        if (preg_match("/\b(\w*$szukaj\w*)\b/ ", $we, $z) == true){
+            $powrot[]= $z[0]."<br>";
+    }
+    }
+    for($i=0, $zwrot=""; $i<count($powrot);$i++){
+        $zwrot .= $powrot[$i];
+    }
+    return $zwrot;
 }
-}
+echo ArraySort($connect, $wyszukaj);
 ?>
