1a362891eeca63137d31ba8640bb883c855d548f
t0mek1 <95845551+t0mek1@users.noreply.github.com>
2022-10-05 11:01:16 +0200

    Delete 2022-4gin-php-intro-main directory

--- 2022-4gin-php-intro-main/tableTool.interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface tableToolInterface {
-    public function __construct($data);
-    public function renderHTML($cols, $filterString='');
-    // public function renderCSV($cols, $filterString='');
-    // public function renderMD($cols, $filterString='');
-}
