107b1222bc6a91f10723650ed42c13ff5492d51b
t0mek1 <95845551+t0mek1@users.noreply.github.com>
2022-10-05 11:04:02 +0200

    Add files via upload

--- tableTool.interface.php
+++ tableTool.interface.php
@@ -2,6 +2,6 @@
 interface tableToolInterface {
     public function __construct($data);
     public function renderHTML($cols, $filterString='');
-    public function renderCSV($cols, $filterString='');
-    public function renderMD($cols, $filterString='');
+    // public function renderCSV($cols, $filterString='');
+    // public function renderMD($cols, $filterString='');
 }
