f5017a6f6f45e16b85cb9a89f9b54f16135cfb1e
Marcin Kwiatkowski <marcin.kwiatkowski@live.zs1mm.edu.pl>
2022-10-05 08:21:27 +0200

    fix

--- tableTool.php
+++ tableTool.php
@@ -12,12 +12,12 @@ $array = explode(' ', file_get_contents('lorem.txt'));
 $table = new tableTool($array);
 
 // Tests
-echo $array->renderHTML(3);
-echo $array->renderHTML(10);
-echo $array->renderHTML(5,'id');
-echo $array->renderCSV(3);
-echo $array->renderCSV(10);
-echo $array->renderCSV(5,'id');
-echo $array->renderMD(3);
-echo $array->renderMD(10);
-echo $array->renderMD(5,'id');
+echo $table->renderHTML(3);
+echo $table->renderHTML(10);
+echo $table->renderHTML(5,'id');
+echo $table->renderCSV(3);
+echo $table->renderCSV(10);
+echo $table->renderCSV(5,'id');
+echo $table->renderMD(3);
+echo $table->renderMD(10);
+echo $table->renderMD(5,'id');
